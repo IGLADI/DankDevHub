5891d7e39c1b2c4b5619dec45cde725a38ca9d87
IGLADI <jarno.roels@gmail.com>
2024-01-12 00:07:08 +0100

    feat/core: updated about page

--- DankDevHub/resources/views/about.blade.php
+++ DankDevHub/resources/views/about.blade.php
@@ -4,6 +4,27 @@
     <body>
         <h1>About Us</h1>
         <p>This is DankDevHub, a place for developers to share their dank memes or other deep stuff like making their own CPU, programming languages,...</p>
+        <p>Our platform is built using the following technologies:</p>
+        <ul>
+            <li>PHP</li>
+            <li>Laravel</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>MySQL</li>
+            <li>Bootstrap</li>
+            <li>MailTrap</li>
+        </ul>
+        <p>Some of our core functionalities include:</p>
+        <ul>
+            <li>User registration and authentication</li>
+            <li>News feed</li>
+            <li>Profile management</li>
+            <li>Threads and post</li>
+            <li>Interactive FAQ</li>
+            <li>Contact form</li>
+            <li>And way more ;)</li>
+        </ul>
         <a href="https://github.com/IGLADI/DankDevHub">GitHub Repository</a>
     </body>
 @endsection
