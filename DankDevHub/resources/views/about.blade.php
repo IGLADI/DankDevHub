e3cfc24d5260390370d4c1b355e53a8f14f29cee
IGLADI <jarno.roels@gmail.com>
2024-01-11 11:32:18 +0100

    feat/core: added github repo to about page

--- DankDevHub/resources/views/about.blade.php
+++ DankDevHub/resources/views/about.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <body>
         <h1>About Us</h1>
-        <p>This is DankDevHub, a place for developers to share their dank memes. or other deap stuff like making their own CPU, programming languages,...</p>
+        <p>This is DankDevHub, a place for developers to share their dank memes or other deep stuff like making their own CPU, programming languages,...</p>
+        <a href="https://github.com/IGLADI/DankDevHub">GitHub Repository</a>
     </body>
 @endsection
