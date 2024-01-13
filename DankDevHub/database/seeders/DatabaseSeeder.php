c925d385333b33d104a5df79c9489dcf67aab748
IGLADI <jarno.roels@gmail.com>
2024-01-13 22:50:44 +0100

    Feat: add NewsSeeder with "Rules"

--- DankDevHub/database/seeders/DatabaseSeeder.php
+++ DankDevHub/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             FAQCategorySeeder::class,
+            NewsSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
